b4b136f61100562b87236a09323bf0206bdd7fe4
xyerakovalskia <xyera123987@gmail.com>
2026-04-20 17:48:14 +0700

    memasukkan prompt untuk menampilkan atribut

--- introductionOOP.cpp
+++ introductionOOP.cpp
@@ -8,3 +8,8 @@ class Mahasiswa {
         float nilai;
 
         void printData(){
+            cout << "NIM : " << nim << endl;
+            cout << "Nama : " << nama << endl;
+            cout << "Nilai : " << nilai << endl;
+        }
+}; //batas class
